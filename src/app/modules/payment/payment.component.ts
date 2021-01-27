6f476b64a54c1220a4f660592fc29e12df85ee1b
Nguyễn Mạnh Cầm <nguyenmanhcam404@gmail.com>
2021-01-27 15:44:11 +0700

    d

--- src/app/modules/payment/payment.component.ts
+++ src/app/modules/payment/payment.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-  validateForm!: FormGroup;
   constructor() { }
 
   ngOnInit() {
   }
-  submitForm(): void {
-    for (const i in this.validateForm.controls) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
-    }
-  }
+
 }
