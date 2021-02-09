3eaf3d76915f8457debeeaa296e8cf2e5611965b
thamnt <thamnt.2403@gmail.com>
2021-02-09 22:12:46 +0700

    commit

--- src/app/_shared/components/product-card/product-card.component.ts
+++ src/app/_shared/components/product-card/product-card.component.ts
@@ -7,19 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductCardComponent {
   isVisible = false;
   isDisplay = false;
-  activeIndex = 0;
   index: number;
-  imgOptions = [
-    {
-      image: '/assets/images/ring1.jpg'
-    },
-    {
-      image: '/assets/images/ring2.jpg'
-    },
-    {
-      image: '/assets/images/ring3.jpg'
-    },
-  ]
   images = [
     {
       thumb: '/assets/images/ring1.jpg',
@@ -28,20 +16,42 @@ export class ProductCardComponent {
   ]
 
   items = [
-
     {
-      image: '/assets/images/ring1.jpg',
-      imageHover: '/assets/images/ring4.jpg',
       name: 'Sun Flower',
-      code: 'SF',
       price: '10,000đ',
-      link: '/',
+      activeIndex: 0,
+      children: [
+        {
+          image: '/assets/images/ring1.jpg',
+          code: 'SF',
+        },
+        {
+          image: '/assets/images/ring3.jpg',
+          code: 'ST',
+        }
+      ]
+    },
+    {
+      name: 'Sun Flower',
+      price: '10,000đ',
+      activeIndex: 0,
+      children: [
+        {
+          image: '/assets/images/ring1.jpg',
+          code: 'SF',
+        },
+        {
+          image: '/assets/images/ring3.jpg',
+          code: 'ST',
+        }
+      ]
     },
   ];
   constructor() { }
-  changeOption(index: number) {
-    this.activeIndex = index;
+  getActiveData(item: any) {
+    return item.children[item.activeIndex]
   }
+
   showModal(): void {
     this.isVisible = true;
   }
